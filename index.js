da77708497913a83aa225036faac8e85b11bcf49
Tej Charfi <charfi@usc.edu>
2020-06-19 22:09:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function sayHiToGrandma(string){
           
           
           else if(string == "I love you, Grandma."){
-            return "I love you, too"
+            return "I love you, too."
           }
           else{
             return "..."
