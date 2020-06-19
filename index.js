975e76683c11682129f224e9e2c69384fde444b9
Tej Charfi <charfi@usc.edu>
2020-06-19 22:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   
         if(string == string.toUpperCase()){
-            return "YES INDEED"
+            return "YES INDEED!"
         }
         else if(string == string.toLowerCase()){
           return "I can't hear you!";
         }
           
           
-          else if(string == "I love you, Grandma"){
+          else if(string == "I love you, Grandma."){
             return "I love you, to"
           }
           else{
